78a49ca671a2f81ad2f842bf1e57772e19a33a5b
ahadsenja <hamdanilm96@gmail.com>
2019-07-16 03:36:34 +0800

    initial commit

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import NavigationBar from '../components/Navbar/Navbar'
+
+const MainPage = () => {
+
+    return (
+        <div>
+            <NavigationBar />
+        </div>
+    )
+}
+
+export default MainPage
