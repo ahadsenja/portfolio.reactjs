867ab407a5a8bd8291651632de0b572af5722f6e
ahadsenja <hamdanilm96@gmail.com>
2019-07-16 14:39:21 +0800

    chore: update header component #5

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Jumbotron } from 'react-bootstrap'
+
+import './header.css'
+
+const Header = () => {
+
+    return (
+        <div>
+            <Jumbotron className='jmb'>
+                <h1>Muhamad Hamdanil</h1>
+            </Jumbotron>
+        </div>
+    )
+}
+
+export default Header
